65623117f6f5f6f6bb2cb48d28d410da63191784
Jeremy Lin <maxlinjr95@gmail.com>
2022-10-29 12:24:50 -0700

    models updated

--- models/comments.js
+++ models/comments.js
@@ -9,7 +9,7 @@ Comments.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
-            autoIncrement: true,
+            autoIncrement: true
         },
         user_id: {
             type: DataTypes.INTEGER,
