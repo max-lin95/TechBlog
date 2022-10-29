abf1dca825bbe2721bce91bd9b5bc1ed6815db3d
Jeremy Lin <maxlinjr95@gmail.com>
2022-10-29 12:24:01 -0700

    dashboardRoutes added for getting all posts

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -3,3 +3,40 @@ const sequelize = require('../config/connection');
 const { Posts, User, Comments } = require('../models');
 const withAuth = require('../utils/auth');
 
+// gets all the posts
+router.get('/', withAuth, (req, res) => {
+    Posts.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'created_at',
+            'id',
+            'title',
+            'posts_content'
+        ],
+        include: [
+            {
+                model: Comments,
+                attributes: [
+                    'id', 'comments_content', 'posts_id', 'user_id', 'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: [
+                        'username'
+                    ]
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostsData.map(posts => posts.get ({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
