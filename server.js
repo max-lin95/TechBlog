d50216d36a044fc687ba53b8dc632c1266614a4f
Jeremy Lin <maxlinjr95@gmail.com>
2022-12-10 11:53:42 -0800

    refactor

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-const session = require('express-session');
 const helpers = require('./utils/helpers');
 
 const app = express();
